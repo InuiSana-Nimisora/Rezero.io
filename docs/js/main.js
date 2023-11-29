d1fd4f282c64ea5c43462b4b657482889ce12981
Oversora <152124969+Oversora@users.noreply.github.com>
2023-11-29 10:58:16 +0800

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -68,4 +68,23 @@ window.onload=function(){
             rightBtn.onclick();
         },1500)
     }
+    var bg = document.getElementsByClassName("box_bg");
+    
+    if((w/h) >= (1920/1080)){
+        for(var i = 0;i < bg.length; i++){
+           bg[i].style.width = w + 'px';
+           bg[i].style.height = w * (1080/1920) + 'px';
+           bg[i].style.top = -(w * (1080/1920) - h)/2 + 'px';
+           bg[i].style.left = '0';
+        }
+    }
+    else{
+        for(var i = 0; i < bg.length; i++){
+            bg[i].style.height = h + 'px';
+            bg[i].style.width = h*(1920/1080) + 'px';
+            bg[i].style.left = -(h*(1920/1080) - w)/2 + 'px';
+            bg[i].style.top = '0';
+        }
+    }
+    
     }
