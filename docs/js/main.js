008234295b218b92b9646c2dca3019bd0fdacab8
Oversora <152124969+Oversora@users.noreply.github.com>
2023-12-07 20:47:11 +0800

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,23 @@
+
 window.onload=function(){
+            // 动画旋转
+        var music = document.getElementById("music");    //获取音乐
+        var musicico = document.getElementById("musicico");   //获取音乐图标
+        var tem = false;  //设置一个变量，用来控制音乐是否在播放。
+        
+        //定义一个函数，当用户单击的时候触发这个函数，从而实现音乐的暂停与播放。
+        musicico.onclick=function(){
+            //tem用来控制音乐当前是否在播放。true代表音乐正在播放，false代表音乐当前正在处于暂停的状态。
+            if(tem == false){
+                music.play();  //播放音乐
+                tem = true;  
+                musicico.style.animationPlayState="running";  //播放音乐图标
+            }else{
+                music.pause();  //暂停音乐
+                musicico.style.animationPlayState="paused"; //暂停音乐图标
+                tem = false;
+            }
+        }
     var items=document.getElementsByClassName("item");
     var circles=document.getElementsByClassName("circle");
     var leftBtn=document.getElementById("btn-left");
@@ -87,5 +106,6 @@ window.onload=function(){
             bg[i].style.top = '0';
         }
     }
-    
+
     }
+
